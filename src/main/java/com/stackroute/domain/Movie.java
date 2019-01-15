f71bc0a533f8b673317251f425d3d34034326579
Komal_R <komalr984@gmail.com>
2019-01-15 16:20:17 +0530

    Task1Commit

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,15 @@
+package com.stackroute.domain;
+
+public class Movie {
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    private Actor actor;
+
+}
