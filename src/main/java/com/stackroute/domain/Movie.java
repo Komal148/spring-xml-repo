7da5cee19ff0b40a557f81781410edb8d6532a7b
Komal_R <komalr984@gmail.com>
2019-01-15 20:37:57 +0530

    Task4CommitAwareInterfaces

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
 
     public Movie()
     {
@@ -21,4 +28,18 @@ public class Movie {
 
     private Actor actor;
 
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+        Actor actor= (Actor) beanFactory.getBean("actorB");
+        System.out.println("From BeanFactoryAware : "+actor);
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("BeanName is : "+s);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Actor actor= (Actor) applicationContext.getBean("actorB");
+        System.out.println("From ApplicationContextAware : "+actor);
+    }
 }
